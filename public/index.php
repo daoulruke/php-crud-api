f17c4fe603e3fa933149cc988234906b0c4a12da
CHRISTOPHER <2224402+daoulruke@users.noreply.github.com>
2022-10-08 07:11:15 +1300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -72,7 +72,7 @@
   $command = '';
   $tables = array();
   $mapping = array();
-  $geometrySrid = null;
+  settype($geometrySrid, 'integer'); 
   $db = new GenericDB($_ENV['PHP_CRUD_API_DRIVER'], $_ENV['PHP_CRUD_API_ADDRESS'], $_ENV['PHP_CRUD_API_PORT'], $_ENV['PHP_CRUD_API_DATABASE'], $command, $tables, $mapping, $_ENV['PHP_CRUD_API_USERNAME'], $_ENV['PHP_CRUD_API_PASSWORD'], $geometrySrid);
   $reflector = new \ReflectionObject($db);
   $db_query = $reflector->getMethod('query');
